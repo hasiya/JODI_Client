d17a62c403896d2a49ac1389a9553e214559f720
Ali Colville <hasith002@yahoo.com>
2016-06-20 17:21:55 +0100

    Trying to get react working

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,8 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import Navbar from './NavbarComponent.js';
+
 
 let yeomanImage = require('../images/yeoman.png');
 
@@ -9,6 +11,7 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
+        <Navbar/>
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
